09e3b84c38e57662c286c95614f43bd72e06bf2e
k <162461123+4UT15T1Ck@users.noreply.github.com>
2025-01-11 23:53:06 +0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const modal0 = document.getElementById('')
+const modal1 = document.getElementsByClassName('')
+const modal2 = document.querySelectorAll()
+const modal3 = document.querySelector(".")
+
+// id in div -> take 1st
+// classname -> list 
+// selector -> take 1st, select like css
+// -------all -> same as selector but list  
+// innerText, HTML -> take text
+// innerHTML -> take all html inside
+// asign value from js to html 
+// ex: a.innerHTML = 'html line'
+// onclick="function-name()"
+// Button.addEventListener('click', function-name)
+// Button.removeEventListener('click', function-name)
+const newItem = document.createElement('div')
+newItem.classList.add('orgnioergoiehoi')
+newItem.textContent = 'ojsownofhwoirf'
+list.appendChild(newItem)
+removeChild
