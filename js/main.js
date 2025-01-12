ce01258d65c8c9dd635d7fc789f22947b02c8271
k <162461123+4UT15T1Ck@users.noreply.github.com>
2025-01-13 00:21:26 +0700

    aaa

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const modal0 = document.getElementById('')
-const modal1 = document.getElementsByClassName('')
-const modal2 = document.querySelectorAll()
-const modal3 = document.querySelector(".")
+// const modal0 = document.getElementById('')
+// const modal1 = document.getElementsByClassName('')
+// const modal2 = document.querySelectorAll()
+// const modal3 = document.querySelector(".")
 
 // id in div -> take 1st
 // classname -> list 
@@ -14,8 +14,25 @@ const modal3 = document.querySelector(".")
 // onclick="function-name()"
 // Button.addEventListener('click', function-name)
 // Button.removeEventListener('click', function-name)
-const newItem = document.createElement('div')
-newItem.classList.add('orgnioergoiehoi')
-newItem.textContent = 'ojsownofhwoirf'
-list.appendChild(newItem)
-removeChild
+// const newItem = document.createElement('div')
+// newItem.classList.add('orgnioergoiehoi')
+// newItem.textContent = 'ojsownofhwoirf'
+// list.appendChild(newItem)
+// removeChild
+function collapse_panel_switch ()
+{
+    const element = document.querySelector('.navbar')
+    if (element.style.maxHeight == '90px') 
+        element.style.maxHeight = '400px'
+    else 
+        element.style.maxHeight = '90px'
+}
+function panel_collapse ()
+{
+    const element = document.querySelector('.navbar')
+    element.style.maxHeight = '90px'
+}
+function section_link_noter ()
+{
+    
+}
